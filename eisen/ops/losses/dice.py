e91e5a2f1b2a9a2ac8f468ed8a0bab1cc85ae6f1
Eisen AI <none@nobody.com>
2020-01-27 20:07:05 +0100

    corrected dice loss

--- eisen/ops/losses/dice.py
+++ eisen/ops/losses/dice.py
@@ -2,7 +2,7 @@ from torch import nn
 
 
 class DiceLoss(nn.Module):
-    def __init__(self, weight=1.0, reduce_dims=None):
+    def __init__(self, weight=1.0, dim=None):
         """
         :param weight: absolute weight of this loss
         :type weight: float
@@ -12,23 +12,22 @@ class DiceLoss(nn.Module):
             {"name": "input_names", "type": "list:string", "value": "['predictions', 'labels']"},
             {"name": "output_names", "type": "list:string", "value": "['dice_loss']"},
             {"name": "weight", "type": "float", "value": "1.0"},
-            {"name": "reduce_dims", "type": "list:int", "value": [1, 2, 3, 4]}
+            {"name": "dim", "type": "list:int", "value": [1, 2, 3, 4]}
         ]
         </json>
         """
         super(DiceLoss, self).__init__()
 
-        if reduce_dims is None:
-            reduce_dims = [1, 2, 3, 4]
+        if dim is None:
+            dim = [1, 2, 3, 4]
 
-        self.reduce_dims = reduce_dims
+        self.dim = dim
 
         self.weight = weight
 
     def forward(self, predictions, labels):
         dice_loss = 1.0 - \
-                    2.0 * (labels * predictions).sum(dim=self.reduce_dims) \
-                    / (labels ** 2 + predictions ** 2).sum(dim=self.reduce_dims)
+                    2.0 * (labels * predictions).sum(dim=self.dim) / (labels ** 2 + predictions ** 2).sum(dim=self.dim)
 
         dice_loss = self.weight * dice_loss.mean()
 
--- eisen/ops/metrics/dice.py
+++ eisen/ops/metrics/dice.py
@@ -2,7 +2,7 @@ from torch import nn
 
 
 class DiceMetric(nn.Module):
-    def __init__(self, weight=1.0, reduce_dims=None):
+    def __init__(self, weight=1.0, dim=None):
         """
         :param weight: absolute weight of this metric
         :type weight: float
@@ -12,22 +12,21 @@ class DiceMetric(nn.Module):
             {"name": "input_names", "type": "list:string", "value": "['predictions', 'labels']"},
             {"name": "output_names", "type": "list:string", "value": "['dice_metric']"},
             {"name": "weight", "type": "float", "value": "1.0"},
-            {"name": "reduce_dims", "type": "list:int", "value": [1, 2, 3, 4]}
+            {"name": "dim", "type": "list:int", "value": [1, 2, 3, 4]}
         ]
         </json>
         """
         super(DiceMetric, self).__init__()
 
-        if reduce_dims is None:
-            reduce_dims = [1, 2, 3, 4]
+        if dim is None:
+            dim = [1, 2, 3, 4]
 
-        self.reduce_dims = reduce_dims
+        self.dim = dim
 
         self.weight = weight
 
     def forward(self, predictions, labels):
-        dice = 2.0 * (labels * predictions).sum(dim=self.reduce_dims) / \
-               (labels ** 2 + predictions ** 2).sum(dim=self.reduce_dims)
+        dice = 2.0 * (labels * predictions).sum(dim=self.dim) / (labels ** 2 + predictions ** 2).sum(dim=self.dim)
 
         dice_metric = self.weight * dice.mean()
 
