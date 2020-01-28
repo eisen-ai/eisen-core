8d9bf8d95b1a04b0b9eb11bfe5326684f5564f11
Eisen AI <none@nobody.com>
2020-01-28 13:15:11 +0100

    updated setup and requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
 setup(
     name='eisen-core',
     version="0.0.1",
     description='Eisen is a collection of tools to train neural networks for medical image analysis',
     packages=find_packages(),
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [],
     },
